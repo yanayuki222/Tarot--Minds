c6aa9d236b6ca936870c1c580685a83742061437
yanayuki222 <a8123222@aoyama.jp>
2024-11-01 00:21:14 +0900

    Add files via upload

--- node.js
+++ node.js
@@ -30,7 +30,7 @@ const cors = require("cors");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const port = 5000;
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 // api/node.js
 
